0095664d7d05d69eae1d6bc03e9c520022d0cd20
pranay <pranay591999@gmail.com>
2023-12-23 16:23:42 +0530

    final backend commit

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,13 @@
 import express from "express";
-
 import cors from 'cors';
-import Routes from "./routes/routes.js";
 import bodyParser from "body-parser";
 import Connection from "./database/db.js";
+import Routes from "./routes/routes.js";
 
 const app = express();
 const PORT = 8000;
 app.use(cors());
+// app.use(express.json({ extended: true })); // or we use bodyParser insted of express 
 app.use(bodyParser.json({extended:true}));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/", Routes);
--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,9 +1,12 @@
 import express from 'express';
+import { addTodo , getAllTodos, toggleTodoDone, updateTodo, deleteTodo} from '../controller/todo-contoller.js';
 
 const routes = express.Router();
 
-routes.post('/todos', (request,response)=>{
-    console.log(request.body);
-})
+routes.post("/todos", addTodo);
+routes.get("/todos", getAllTodos);
+routes.get("/todos/:id", toggleTodoDone);
+routes.put("/todos/:id", updateTodo);
+routes.delete("/todos/:id", deleteTodo);
 
 export default routes;
